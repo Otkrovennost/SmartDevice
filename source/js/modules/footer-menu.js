db5dc08220c45517fb12e41404154040b453a5ec
Otkrovennost <Otkrovennost@mail.ru>
2020-06-08 23:00:55 +0300

    реализация футера

---
+++ source/js/modules/footer-menu.js
@@ -0,0 +1,46 @@
+const footerBlock = document.querySelector(`.footer`);
+const footerNavBlock = footerBlock.querySelector(`.footer__nav`);
+const footerContactsBlock = footerBlock.querySelector(`.footer__contacts`);
+
+export const getDefaultState = () => {
+  footerNavBlock.classList.add(`footer__nav--closed`);
+  footerContactsBlock.classList.add(`footer__contacts--closed`);
+};
+
+const showFooterNavBlock = () => {
+  footerNavBlock.classList.remove(`footer__nav--closed`);
+  footerNavBlock.classList.add(`footer__nav--opened`);
+};
+
+const hideFooterNavBlock = () => {
+  footerNavBlock.classList.remove(`footer__nav--opened`);
+  footerNavBlock.classList.add(`footer__nav--closed`);
+};
+
+const showFooterContactsBlock = () => {
+  footerContactsBlock.classList.remove(`footer__contacts--closed`);
+  footerContactsBlock.classList.add(`footer__contacts--opened`);
+};
+
+const hideFooterContactsBlock = () => {
+  footerContactsBlock.classList.remove(`footer__contacts--opened`);
+  footerContactsBlock.classList.add(`footer__contacts--closed`);
+};
+
+export const onClickNavToggler = () => {
+  if (footerContactsBlock.classList.contains(`footer__contacts--opened`) || footerNavBlock.classList.contains(`footer__nav--closed`)) {
+    showFooterNavBlock();
+    hideFooterContactsBlock();
+  } else {
+    hideFooterNavBlock();
+  }
+};
+
+export const onClickContactsToggler = () => {
+  if (footerNavBlock.classList.contains(`footer__nav--opened`) || footerContactsBlock.classList.contains(`footer__contacts--closed`)) {
+    showFooterContactsBlock();
+    hideFooterNavBlock();
+  } else {
+    hideFooterContactsBlock();
+  }
+};
--- source/js/modules/index.js
+++ source/js/modules/index.js
@@ -1,10 +1,16 @@
 import {popupOpenHandler, saveInLocalStorage} from "./popup.js";
 import {scrollToContent} from "./scroll.js";
+import {getDefaultState, onClickNavToggler, onClickContactsToggler} from "./footer-menu.js";
 
 const openUserMessageLink = document.querySelector(`.nav__call`);
 const formMessageBlock = document.querySelector(`.form-call`);
 const submitForm = formMessageBlock.querySelector(`form`);
 const scrollPreviewLink = document.querySelector(`.preview__link-scroll`);
+const footerBlock = document.querySelector(`.footer`);
+const navToggler = footerBlock.querySelector(`.footer__toggler--nav`);
+const contactsToggler = footerBlock.querySelector(`.footer__toggler--contacts`);
+
+getDefaultState();
 
 if (openUserMessageLink) {
   openUserMessageLink.addEventListener(`click`, popupOpenHandler);
@@ -16,4 +22,12 @@ if (submitForm) {
 
 if (scrollPreviewLink) {
   scrollToContent();
+}
+
+if (navToggler) {
+  navToggler.addEventListener(`click`, onClickNavToggler);
+}
+
+if (contactsToggler) {
+  contactsToggler.addEventListener(`click`, onClickContactsToggler);
 }
