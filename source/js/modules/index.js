ae0b945c23869d95dbfca4a5b19b345711bbd529
Otkrovennost <Otkrovennost@mail.ru>
2020-06-09 23:41:19 +0300

    внесение правок

--- source/js/modules/index.js
+++ source/js/modules/index.js
@@ -7,7 +7,6 @@ const popupCallForm = document.querySelector(`.form-call`);
 const submitForm = popupCallForm.querySelector(`form`);
 const scrollPreviewLink = document.querySelector(`.preview__link-scroll`);
 const footerBlock = document.querySelector(`.footer`);
-const popupElement = document.querySelector(`.popup`);
 const navToggler = footerBlock.querySelector(`.footer__toggler--nav`);
 const contactsToggler = footerBlock.querySelector(`.footer__toggler--contacts`);
 
